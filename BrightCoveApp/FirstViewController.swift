3f3eb127a53283d978714584c3ed0fbdbfc122ac
Ricardo Rabeto <ricardomarabeto@gmail.com>
2019-10-15 19:00:37 +0100

    Initial Commit

---
+++ BrightCoveApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BrightCoveApp
+//
+//  Created by Ricardo Rabeto on 15/10/2019.
+//  Copyright © 2019 Ricardo Rabeto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BrightCoveApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BrightCoveApp
+//
+//  Created by Ricardo Rabeto on 15/10/2019.
+//  Copyright © 2019 Ricardo Rabeto. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
